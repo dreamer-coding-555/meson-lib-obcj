acedbdae4be16cabb3a32c72a7ab9da220d05ec2
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2024-05-10 10:02:52 -0600

    Update xtest_cases.m

--- test/xtest_cases.m
+++ test/xtest_cases.m
@@ -19,20 +19,39 @@ Description:
 // XUNIT-CASES: list of test cases testing project features
 //
 
-XTEST(lib_subtract_case) {
-    TEST_ASSUME_TRUE(subtract(4, 2) == 2);
-    TEST_ASSUME_FALSE(subtract(2, 55) == 2);
+//
+// XUNIT-CASES: list of test cases testing project features
+//
+
+XTEST(FossilMath_subtract_case) {
+    TEST_ASSUME_TRUE([FSCLFossilMath subtractWithInteger:4 andInteger:2] == 2);
+    TEST_ASSUME_FALSE([FSCLFossilMath subtractWithInteger:2 andInteger:55] == 2);
 }
 
-XTEST(lib_add_case) {
-    TEST_ASSUME_TRUE(add(2, 2) == 4);
-    TEST_ASSUME_FALSE(add(2, 3) == 42);
+XTEST(FossilMath_add_case) {
+    TEST_ASSUME_TRUE([FSCLFossilMath addWithInteger:2 andInteger:2] == 4);
+    TEST_ASSUME_FALSE([FSCLFossilMath addWithInteger:2 andInteger:3] == 42);
+}
+
+XTEST(FossilMath_multiply_case) {
+    TEST_ASSUME_TRUE([FSCLFossilMath multiplyWithInteger:2 andInteger:3] == 6);
+    TEST_ASSUME_FALSE([FSCLFossilMath multiplyWithInteger:4 andInteger:5] == 21);
+}
+
+XTEST(FossilMath_divide_case) {
+    TEST_ASSUME_TRUE([FSCLFossilMath divideWithDouble:10.0 byDouble:2.0] == 5.0);
+    TEST_ASSUME_TRUE([FSCLFossilMath divideWithDouble:15.0 byDouble:3.0] == 5.0);
+    TEST_ASSUME_TRUE([FSCLFossilMath divideWithDouble:25.0 byDouble:5.0] == 5.0);
+    TEST_ASSUME_TRUE([FSCLFossilMath divideWithDouble:0.0 byDouble:5.0] == 0.0);
+    TEST_ASSUME_FALSE([FSCLFossilMath divideWithDouble:10.0 byDouble:0.0] == 0.0);
 }
 
 //
 // XUNIT-GROUP: a group of test cases from the current test file
 //
 XTEST_DEFINE_POOL(basic_group) {
-    XTEST_RUN_UNIT(lib_subtract_case);
-    XTEST_RUN_UNIT(lib_add_case);
+    XTEST_RUN_UNIT(FossilMath_subtract_case);
+    XTEST_RUN_UNIT(FossilMath_add_case);
+    XTEST_RUN_UNIT(FossilMath_multiply_case);
+    XTEST_RUN_UNIT(FossilMath_divide_case);
 } // end of fixture
