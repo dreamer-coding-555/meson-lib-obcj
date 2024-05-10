c2d27aa52f293a51cf7e3d9e75f6dc794ea7b0a6
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2024-05-10 05:40:31 -0600

    Update and rename module.c to module.m

--- code/source/module.m
+++ code/source/module.m
@@ -10,7 +10,7 @@ Description:
     feel free to contact Michael at michaelbrockus@gmail.com.
 ==============================================================================
 */
-#include "fossil/module.h"
+#import "fossil/module.h"
 
 
 int add(int a, int b) {
